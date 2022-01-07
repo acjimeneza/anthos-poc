7220b1a9fe00b60b9df8f7be23533bc3a1f75fc9
Andres J <acja87@hotmail.com>
2022-01-07 12:51:56 -0500

    Se actualiza el uso de variables en el modulo de istio

--- modules/istio/main.tf
+++ modules/istio/main.tf
@@ -15,11 +15,7 @@ provider "helm" {
 
 resource "helm_release" "istio_base" {
   name = "istio-base"
-  # repository = "https://istio-release.storage.googleapis.com/charts"
-  # chart      = "base"
-  # dependency_update = true
   namespace = "istio-system"
-  # version = "1.12.1"
   chart = "${path.module}/manifest/base"
 
   depends_on = [
@@ -28,33 +24,25 @@ resource "helm_release" "istio_base" {
 }
 
 resource "helm_release" "istio_control" {
-  name = "istiod"
-  # repository = "https://istio-release.storage.googleapis.com/charts"
-  # chart      = "istiod"
-  # dependency_update = true
+  name      = "istiod"
   namespace = "istio-system"
-  # version = "1.12.1"
-  timeout = 300
-  chart   = "${path.module}/manifest/istio-control/istio-discovery"
+  timeout   = 300
+  chart     = "${path.module}/manifest/istio-control/istio-discovery"
 
   # Resources for sidecar
-  set {
-    name  = "global.proxy.resources.requests.cpu"
-    value = var.sidecar_request_cpu
-  }
-
-  set {
-    name  = "global.proxy.resources.requests.memory"
-    value = var.sidecar_request_memory
-  }
-  set {
-    name  = "global.proxy.resources.limits.cpu"
-    value = var.sidecar_limit_cpu
-  }
-  set {
-    name  = "global.proxy.resources.limits.memory"
-    value = var.sidecar_limit_memory
-  }
+  values = [
+    <<EOT
+global:
+  proxy:
+    resources:
+      requests:
+        cpu: ${var.sidecar_request_cpu}
+        memory: ${var.sidecar_request_memory}
+      limits:
+        cpu: ${var.sidecar_limit_cpu}
+        memory: ${var.sidecar_limit_memory}
+EOT
+  ]
 
   depends_on = [
     helm_release.istio_base,
@@ -62,42 +50,12 @@ resource "helm_release" "istio_control" {
 }
 
 resource "helm_release" "istio_ingress" {
-  name = "istio-ingress"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "gateway"
-  dependency_update = true
-  version = "1.12.1"
-
+  name            = "istio-ingress"
   namespace       = "istio-ingress"
-  # chart           = "${path.module}/manifest/istio-ingress"
+  chart           = "${path.module}/manifest/istio-ingress"
   cleanup_on_fail = true
   force_update    = true
 
-  # Resources for istio ingress
-  # set {
-  #   name  = "gateways.istio-ingressgateway.autoscaleMin"
-  #   value = var.ingressgateway_autoscale_min
-  # }
-  # set {
-  #   name  = "gateways.istio-ingressgateway.autoscaleMax"
-  #   value = var.ingressgateway_autoscale_max
-  # }
-  # set {
-  #   name  = "gateways.istio-ingressgateway.resources.requests.cpu"
-  #   value = var.ingressgateway_request_cpu
-  # }
-  # set {
-  #   name  = "gateways.istio-ingressgateway.resources.requests.memory"
-  #   value = var.ingressgateway_request_memory
-  # }
-  # set {
-  #   name  = "gateways.istio-ingressgateway.resources.limits.cpu"
-  #   value = var.ingressgateway_limit_cpu
-  # }
-  # set {
-  #   name  = "gateways.istio-ingressgateway.resources.limits.memory"
-  #   value = var.ingressgateway_limit_memory
-  # }
   values = [
     <<EOT
 gateways:
